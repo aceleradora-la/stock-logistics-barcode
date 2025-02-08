3be876e10d193da8bcf2cdfd69fa9800ba7c8e23
edescalona <ediliodevelop1991@gmail.com>
2025-02-07 21:38:04 -0500

    [16.0][IMP] stock_barcodes_quality_control: Tests

--- stock_barcodes_quality_control/tests/test_barcodes_quality_control.py
+++ stock_barcodes_quality_control/tests/test_barcodes_quality_control.py
@@ -8,3 +8,12 @@ class TestCommonStockBarcodes(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.WizardQualityControl = cls.env["quality.control.validate.wizard"]
+        cls.WizardPicking = cls.env["wiz.stock.barcodes.read.picking"]
+        cls.wizard_control = cls.WizardQualityControl.create({})
+        cls.wizard_picking = cls.WizardPicking.create({})
+
+    def test_action_validate_quality_control(self):
+        with self.assertRaises(KeyError):
+            self.wizard_control.action_validate_quality_control()
+        self.assertFalse(self.wizard_picking.show_quantity_control())
