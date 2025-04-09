e55b672fc861abfc5256f4180c580f823caac2ed
aceleradora.la <info@aceleradora.la>
2025-04-09 10:50:38 -0300

    Update barcode_handler_field.esm.js

--- stock_barcodes/static/src/utils/barcode_handler_field.esm.js
+++ stock_barcodes/static/src/utils/barcode_handler_field.esm.js
@@ -1,23 +1,28 @@
 /** @odoo-module **/
-import {BarcodeHandlerField} from "@barcodes/barcode_handler_field";
-import {patch} from "@web/core/utils/patch";
-import {useService} from "@web/core/utils/hooks";
-const {useEffect} = owl;
+import { BarcodeHandlerField } from "@barcodes/barcode_handler_field";
+import { patch } from "@web/core/utils/patch";
+import { useService } from "@web/core/utils/hooks";
+const { useEffect } = owl;
 
 patch(BarcodeHandlerField, {
-    /* eslint-disable no-unused-vars */
     setup() {
-        this._super(...arguments);
+        // Llamamos al setup original si existe
+        if (super.setup) {
+            super.setup();
+        }
+
         const busService = useService("bus_service");
         this.orm = useService("orm");
-        const notifyChanges = async ({detail: notifications}) => {
-            for (const {payload, type} of notifications) {
+
+        const notifyChanges = async ({ detail: notifications }) => {
+            for (const { payload, type } of notifications) {
                 if (type === "stock_barcodes_refresh_data") {
                     await this.env.model.root.load();
                     this.env.model.notify();
                 }
             }
         };
+
         useEffect(() => {
             busService.addChannel("barcode_reload");
             busService.addEventListener("notification", notifyChanges);
@@ -27,8 +32,13 @@ patch(BarcodeHandlerField, {
             };
         });
     },
+
     onBarcodeScanned(event) {
-        this._super(...arguments);
+        // También acá, usamos super.onBarcodeScanned si existe
+        if (super.onBarcodeScanned) {
+            super.onBarcodeScanned(event);
+        }
+
         if (this.props.record.resModel.includes("wiz.stock.barcodes.read")) {
             $("#dummy_on_barcode_scanned").click();
         }
