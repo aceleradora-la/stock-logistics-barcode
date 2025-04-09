fd9d340aa7f43c562a6fba58144ba3f8be0ad7c2
aceleradora.la <info@aceleradora.la>
2025-04-09 10:57:36 -0300

    Update numeric_step.esm.js

--- stock_barcodes/static/src/widgets/numeric_step.esm.js
+++ stock_barcodes/static/src/widgets/numeric_step.esm.js
@@ -1,37 +1,32 @@
-/** @odoo-module */
+/** @odoo-module **/
 /* Copyright 2022 Tecnativa - Alexandre D. Díaz
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-import {NumericStep} from "@web_widget_numeric_step/numeric_step.esm";
-import {isAllowedBarcodeModel} from "../utils/barcodes_models_utils.esm";
-import {patch} from "@web/core/utils/patch";
+import { NumericStep } from "@web_widget_numeric_step/numeric_step.esm";
+import { isAllowedBarcodeModel } from "../utils/barcodes_models_utils.esm";
+import { patch } from "@web/core/utils/patch";
 
 patch(NumericStep.prototype, {
     _onFocus() {
         if (isAllowedBarcodeModel(this.props.record.resModel)) {
-            // Auto select all content when user enters into fields with this
-            // widget.
-            this.inputRef.el.select();
+            // Auto select all content when user enters into fields with this widget.
+            if (this.inputRef?.el?.select) {
+                this.inputRef.el.select();
+            }
         }
     },
 
     _onKeyDown(ev) {
-        if (isAllowedBarcodeModel(this.props.record.resModel) && ev.keyCode === 13) {
-            const action_confirm = document.querySelector(
-                "button[name='action_confirm']"
-            );
-
-            if (action_confirm) {
-                action_confirm.click();
+        if (isAllowedBarcodeModel(this.props.record.resModel) && ev.key === "Enter") {
+            const actionConfirm = document.querySelector("button[name='action_confirm']");
+            if (actionConfirm) {
+                actionConfirm.click();
                 return;
             }
 
-            const action_confirm_force = document.querySelector(
-                "button[name='action_force_done']"
-            );
-
-            if (action_confirm_force) {
-                action_confirm_force.click();
+            const actionConfirmForce = document.querySelector("button[name='action_force_done']");
+            if (actionConfirmForce) {
+                actionConfirmForce.click();
                 return;
             }
         }
