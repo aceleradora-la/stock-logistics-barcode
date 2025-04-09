b8e783f2248f7d05e9538c99b458aae2ad2af862
aceleradora.la <info@aceleradora.la>
2025-04-09 17:24:48 -0300

    Update stock_barcodes_read_form_controller.esm.js

--- stock_barcodes_camera/static/src/views/barcode_scanner/stock_barcodes_read_form_controller.esm.js
+++ stock_barcodes_camera/static/src/views/barcode_scanner/stock_barcodes_read_form_controller.esm.js
@@ -2,13 +2,14 @@
 
 import { FormController } from '@web/views/form/form_controller';
 import { registry } from '@web/core/registry';
-import { useService } from '@web/core/utils/hooks';
+import { useService, useModel } from '@web/core/utils/hooks';
 
 export class StockBarcodesReadFormController extends FormController {
     setup() {
         super.setup();
         this.barcodeScanner = useService('barcode_scanner');
         this.openCamera = this.openCamera.bind(this);
+        this.model = useModel();
     }
 
     async openCamera() {
@@ -36,9 +37,22 @@ export class StockBarcodesReadFormController extends FormController {
         if (openCameraButton) {
             openCameraButton.addEventListener('click', this.openCamera);
         }
+        this.updateBarcodeScannerVisibility();
+    }
+
+    async updateBarcodeScannerVisibility() {
+        const barcodeContainer = this.el.querySelector('#barcode_scanner_container');
+        const showScanner = this.model.root.data.show_barcode_scanner;
+        if (barcodeContainer) {
+            barcodeContainer.style.display = showScanner ? 'block' : 'none';
+        }
+    }
+
+    willUpdateProps(nextProps) {
+        super.willUpdateProps(nextProps);
+        this.updateBarcodeScannerVisibility();
     }
 }
 
-StockBarcodesReadFormController.template = 'stock_barcodes_camera.StockBarcodesReadFormView'; // Asegúrate de que este template exista o elimínalo si no es necesario
-
+StockBarcodesReadFormController.template = 'stock_barcodes_camera.StockBarcodesReadFormView'; // Ajusta si es necesario
 registry.category('views').add('wiz_stock_barcodes_read_form', StockBarcodesReadFormController);
